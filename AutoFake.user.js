d115e3c56a253f1f74e8881b16a20d7362730a70
JakubMikolajczyk <Mikolajczyk.Jakub@outlook.com>
2023-11-11 16:36:38 +0100

    Update AutoFake.user.js

--- AutoFake.user.js
+++ AutoFake.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Auto fake
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
-// @author       You
+// @author       Jakub Mikolajczyk
 // @match        https://*.plemiona.pl/game.php?*&screen=place
 // @match        https://*.plemiona.pl/game.php?*&screen=place*
 // @match        https://*.plemiona.pl/game.php?screen=place
@@ -13,74 +13,87 @@
 // @grant        none
 // ==/UserScript==
 
-let ignore = ['spear', 'sword', 'archer', 'marcher', 'knight', 'militia', 'snob']
+class Unit{
+ constructor(input){
+ this.input = input
+ }
 
-let population = {
-    "axe": 1,
-    "spy": 2,
-    "light": 4,
-    "heavy": 6,
-    "ram": 5,
-    "catapult": 8
+    getMaxVal(){
+     return this.input.attr('data-all-count')
+    }
+
+    setInputVal(val){
+    this.input.val(Math.min(this.getMaxVal(), Math.max(0, Math.ceil(val))))
+    }
+
+    setMaxInputVal(){
+        this.setInputVal(this.getMaxVal())
+    }
+
+    setMaxInputValWithOffset(offset){
+        this.setInputVal(this.getMaxVal() - offset)
+    }
+
+    getInputVal(){
+        return Number(this.input.val())    }
+
+    addToInputVal(val){
+     this.setInputVal(this.getInputVal() + val)
+    }
 }
 
+function getUnits(){
 
-let fake_limit
-
-function getInputs(){
-
-    let inputs = {}
+    let units = {}
     game_data.units.forEach(unit => {
-        if (!ignore.includes(unit))
-            inputs[unit] = $(`#unit_input_${unit}`)
+            units[unit] = new Unit($(`#unit_input_${unit}`))
     })
-      return inputs
+      return units
 }
 
 (function() {
     'use strict';
 
-    let inputs = getInputs()
-    fake_limit = Math.ceil(game_data.village.points * 0.01)
-    console.log(inputs)
+    let units = getUnits()
+    let fake_limit = Math.ceil(game_data.village.points * 0.01)
 
-    if(inputs["ram"].attr('data-all-count') > 0){
-        inputs["ram"].val(1)
-        fake_limit -= population["ram"]
+    units.spy.setInputVal(5)
+    fake_limit -= units.spy.getInputVal() * 2
+
+    if(units.ram.getMaxVal() > 0){
+        units.ram.setInputVal(1)
+        fake_limit -= 5
     }
-    else{
-        inputs["catapult"].val(1)
-        fake_limit -= population["catapult"]
-    }
-
-    let spyNum = Math.min(inputs["spy"].attr('data-all-count'), 5)
-    inputs["spy"].val(spyNum)
-    fake_limit -= population["spy"] * spyNum
-
-    console.log(fake_limit)
-
-    let pop = inputs["axe"].attr('data-all-count') * population["axe"] +
-        inputs["light"].attr('data-all-count') * population["light"]
-
-    if(inputs["heavy"].attr('data-all-count') > 1000){
-        pop += inputs["heavy"].attr('data-all-count') * population["heavy"]
-
-        let ckNum = Math.ceil(inputs["heavy"].attr('data-all-count') * population["heavy"] / pop * fake_limit /population["heavy"])
-        inputs["heavy"].val(ckNum)
+    else if (units.catapult.getMaxVal() > 0){
+        units.catapult.setInputVal(1)
+        fake_limit -= 8
     }
 
-    console.log(fake_limit)
 
-    let axeNum = Math.ceil(inputs["axe"].attr('data-all-count') * population["axe"] / pop * fake_limit/population["axe"])
-        inputs["axe"].val(axeNum)
+    let axe_num = fake_limit/2
+    let lk_num = fake_limit/8
 
-    console.log(fake_limit)
+    units.axe.setInputVal(axe_num)
+    fake_limit -= units.axe.getInputVal()
 
-    let lkNum = Math.ceil(inputs["light"].attr('data-all-count') * population["light"] / pop * fake_limit/population["light"])
-        inputs["light"].val(lkNum)
+    units.light.setInputVal(lk_num)
+    fake_limit -= units.light.getInputVal() * 4
 
-    console.log(fake_limit)
-   // let inputs = getInputs()
-   // console.log(inputs)
-   // inputs["spy"].val(20)
+    if(fake_limit > 0){
+        units.axe.addToInputVal(fake_limit)
+        fake_limit -= units.axe.getInputVal()
+    }
+
+    if(fake_limit > 0){
+        units.light.addToInputVal(fake_limit/4)
+        fake_limit -= units.light.getInputVal()
+    }
+    if(fake_limit > 0 && (units.spy.getMaxVal() - fake_limit/2) > 50){
+        units.spy.addToInputVal(fake_limit/2)
+    }
+
+    if(fake_limit > 0 && (units.heavy.getMaxVal() - fake_limit/6) > 200){
+        units.heave.setInputVal(fake_limit/6)
+    }
+
 })();
